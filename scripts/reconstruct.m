08c06535723855762c88cf81418d971a21ca3892
Arunabh Ghosh_150070006_ <arunabhghosh@rudra.ee.iitb.ac.in>
2018-03-13 16:22:09 +0530

    Get all results

--- scripts/reconstruct.m
+++ scripts/reconstruct.m
@@ -1,17 +1,17 @@
-num_theta = [3 5 10 20 50 100 150];
+num_theta = [3 5 10 20 30 50 80 100 150];
 % Get the image.
 P = imread('../images/200px-mickey.jpg');
 P = imresize(P, 0.4);
 P = im2double(rgb2gray(P));
-parfor o=1:7
+parfor o=1:9
     disp(num_theta(o));
     % Write the original image.
-    imwrite(P, strcat('../results/unknown_angles/100/',...
+    imwrite(P, strcat('../results/moment_estimation/noisy_angles/170/',...
         num2str(num_theta(o)), '/original_image.png'));
 
     % Constants.
     snr = 1;
-    num_angles = 100;
+    num_angles = 170;
     precision = 1;
 
     % Define ground truth angles and take the tomographic projection.
@@ -35,12 +35,12 @@ parfor o=1:7
     % Reconstruct the images from projection.
     reconstructed_image = iradon(projections, thetasestimated, output_size);
     imwrite(reconstructed_image, ...
-        strcat('../results/unknown_angles/100/',...
+        strcat('../results/moment_estimation/noisy_angles/170/',...
         num2str(num_theta(o)), '/estimated_image.png'));
 
     reconstructed_image = refine_reconstruction(projections,...
         thetasestimated', height, width, projection_length, output_size);
 
-    imwrite(reconstructed_image, strcat('../results/unknown_angles/100/',...
+    imwrite(reconstructed_image, strcat('../results/moment_estimation/noisy_angles/170/',...
         num2str(num_theta(o)), '/reconstructed.png'));
 end;
